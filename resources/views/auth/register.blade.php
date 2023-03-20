4a425889352ddd2b6f2f21c597ff22d32830d21c
Novavalentino <novavalentino925@gmail.com>
2023-03-20 09:54:29 +0700

    login register view

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register </title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container"><br>
+        <div class="col-md-4 col-md-offset-4">
+            <h2 class="text-center"><b>Daftar</b><br></h3>
+            <hr>
+            @if(session('error'))
+            <div class="alert alert-danger">
+                <b>Opps!</b> {{session('error')}}
+            </div>
+            @endif
+            <form action="{{ route('masyarakat.store') }}" method="post">
+            @csrf
+                <div class="form-group">
+                    <label>Nik</label>
+                    <input type="nik" name="nik" class="form-control" placeholder="Nik" required>
+                </div>
+                <div class="form-group">
+                    <label>Nama</label>
+                    <input type="nama" name="nama" class="form-control" placeholder="Nama" required>
+                </div>
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="username" name="username" class="form-control" placeholder="Username" required>
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" class="form-control" placeholder="Password" required>
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                <hr>
+                <p class="text-center">Sudah punya akun? <a href="#">Login</a> sekarang!</p>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
