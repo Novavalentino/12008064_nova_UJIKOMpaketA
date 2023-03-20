ba7d4ecb6b3978a0302cc27c0c2b4d483037e640
Novavalentino <novavalentino925@gmail.com>
2023-03-20 15:15:40 +0700

    update controller

--- app/Http/Controllers/MasyarakatController.php
+++ app/Http/Controllers/MasyarakatController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Masyarakat;
+use Illuminate\Testing\Fluent\Concerns\Hash;
 use Illuminate\Http\Request;
 
 class MasyarakatController extends Controller
---
+++ app/Http/Controllers/PengaduanController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pengaduan;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Masyarakat;
+use App\Models\Petugas;
+use Illuminate\Http\Request;
+
+class PengaduanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pengaduans = Pengaduan::latest()->paginate(5);
+        return view('pengaduan.index', compact('pengaduans'))->with('i', (request()->input('page', 1)-1)*5);
+    }
+    public function indexmas(){
+        $pengaduans = Pengaduan::where('nik', Auth::guard('masyarakat')->user()->nik)->get();
+        return view('pengaduan.masyarakat', compact('pengaduans'))->with('i', (request()->input('page', 1)-1)*5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'judul_laporan'=>'required',
+            'tgl_pengaduan'=>'required',
+            'nik'=>'required',
+            'isi_laporan'=>'required',
+            'foto'=>'image|file|required',
+            'status',
+        ]);
+
+        $validated['status']='0';
+        $validated['foto']= $request->file('foto')->store('post-images');
+        Pengaduan::create($validated);
+        return redirect('dashboard')->with('success', 'Laporanmu sudah kami terima, silahkan tunggu konfirmasi lebih lanjut.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pengaduan  $pengaduan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pengaduan $pengaduan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pengaduan  $pengaduan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pengaduan $pengaduan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pengaduan  $pengaduan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pengaduan $pengaduan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pengaduan  $pengaduan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pengaduan $pengaduan)
+    {
+        //
+    }
+
+    public function proses($id_pengaduan){
+        $pengaduan = Pengaduan::find($id_pengaduan);
+        $pengaduan->update(['status'=>'proses']);
+        return redirect('pengaduan');
+    }
+}
